c483ca110c3a5d3606c5f0acbcd416426c6aeea5
John-Henry Liberty <johnhenry.liberty@gmail.com>
2015-09-12 17:13:04 -0500

    Updates router generator to use this.route instead of this.resource, updates index route file to show model before actions, adds createdAt/updatedAt to model by default and mokes it a const instead of var

--- blueprints/scaffold-route/files/__root__/__path__/__name__/index.js
+++ blueprints/scaffold-route/files/__root__/__path__/__name__/index.js
@@ -1,14 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return this.store.find('<%= dasherizedModuleName %>');
+  },
   actions: {
     remove: function(model) {
       if(confirm('Are you sure?')) {
         model.destroyRecord();
       }
     }
-  },
-  model: function() {
-    return this.store.find('<%= dasherizedModuleName %>');
   }
 });
